90120db8e42fd2b6acd5b59e665ef16624b27517
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-11-26 19:37:19 +0600

    zod validation check

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Application } from "express";
 import cors from "cors";
 import routeNotFound from "./app/middlewares/routeNotFound";
+import router from "./app/routes";
 
 
 const app: Application = express();
@@ -8,6 +9,9 @@ const app: Application = express();
 // parser
 app.use(cors());
 app.use(express.json());
+
+// all routes
+app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server Is Running!");
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { UserValidation } from "./user.validation";
 
 const router = Router();
 
 router.post(
   '/create-user',
+  validateRequest(UserValidation.userValidationSchema),
   UserControllers.createUser
 );
 
